8fb1293784b9bffcc42750e226400e0cdfafae63
vitao-loft <vitor.santos@loft.com.br>
2023-03-09 08:02:06 -0300

    Inserindo src

---
+++ models/staging/raw_data_stone/stg_raw_data_stone__transacoes_usuarios.sql
@@ -0,0 +1,12 @@
+SELECT
+     "codigo da transacao"           AS cd_transacao
+    ,"data e hora da transacao"      AS tm_transacao
+    ,"metodo de captura"             AS nm_captura
+    ,"bandeira do cartao"            AS nm_bandeira_cartao
+    ,"metodo de pagamento"           AS nm_metodo_pagamento
+    ,"estado da transacao"           AS nm_estado_transacao
+    ,"valor da transacao"            AS vl_transacao
+    ,"codigo do usuario"             AS cd_usuario
+    ,"estado do usuario"             AS nm_estado_usuario
+    ,"cidade do usuario"             AS nm_cidade_usuario
+FROM {{SOURCE('','')}}
